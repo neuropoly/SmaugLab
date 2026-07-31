ec53600df4a11280b3ede00046a44c34278035a0
Nathan Molinier <nathan.molinier@polymtl.ca>
2026-07-31 16:23:44 -0400

    add new validation augmentation parameter

--- auglab/trainers/nnUNetTrainerDAExt.py
+++ auglab/trainers/nnUNetTrainerDAExt.py
@@ -169,6 +169,11 @@ class nnUNetTrainerDAExtGPU(nnUNetTrainer):
         self.transforms = AugTransformsGPU(json_path=json_path).to(self.device)
         print(f'Using AugLab GPU transforms with parameters from: {json_path}')
 
+        # Load JSON parameters for validation augmentation checkpoints
+        with open(json_path, 'r') as f:
+            config = json.load(f)
+        self.validation_augmentation_checkpoints = config.get("ValidationAugmentationCheckpoints", {}).get("checkpoints", [0])
+
         # Copy json transfrom parameters to output folder
         shutil.copy(
             json_path,
@@ -376,18 +381,84 @@ class nnUNetTrainerDAExtGPU(nnUNetTrainer):
             self.optimizer.step()
         return {'loss': l.detach().cpu().numpy()}
 
+    def validation_step(self, batch: dict, augmentation_prob: float) -> dict:
+            data = batch['data']
+            target = batch['target']
+    
+            data = data.to(self.device, non_blocking=True)
             # Now target should be a single tensor, not a list
             target = target.to(self.device, non_blocking=True)
             # if isinstance(target, list):
             #     target = [i.to(self.device, non_blocking=True) for i in target]
             # else:
             #     target = target.to(self.device, non_blocking=True)
+    
+            # Autocast can be annoying
+            # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+            # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+            # So autocast will only be active if we have a cuda device.
+            with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
+                # Apply GPU augmentations to full-resolution data/target
+                if torch.rand(1).item() < augmentation_prob:
+                    data, target = self.transforms(data, target)
+    
                 # Create multi-scale targets for deep supervision after augmentation
                 deep_supervision_scales = self._get_deep_supervision_scales()
                 if deep_supervision_scales is not None:
                     ds_transform = DownsampleSegForDSTransformCustom(ds_scales=deep_supervision_scales)
                     target = ds_transform(target)
 
+                output = self.network(data)
+                del data
+                l = self.loss(output, target)
+    
+            # we only need the output with the highest output resolution (if DS enabled)
+            if self.enable_deep_supervision:
+                output = output[0]
+                target = target[0]
+    
+            # the following is needed for online evaluation. Fake dice (green line)
+            axes = [0] + list(range(2, output.ndim))
+    
+            if self.label_manager.has_regions:
+                predicted_segmentation_onehot = (torch.sigmoid(output) > 0.5).long()
+            else:
+                # no need for softmax
+                output_seg = output.argmax(1)[:, None]
+                predicted_segmentation_onehot = torch.zeros(output.shape, device=output.device, dtype=torch.float32)
+                predicted_segmentation_onehot.scatter_(1, output_seg, 1)
+                del output_seg
+    
+            if self.label_manager.has_ignore_label:
+                if not self.label_manager.has_regions:
+                    mask = (target != self.label_manager.ignore_label).float()
+                    # CAREFUL that you don't rely on target after this line!
+                    target[target == self.label_manager.ignore_label] = 0
+                else:
+                    if target.dtype == torch.bool:
+                        mask = ~target[:, -1:]
+                    else:
+                        mask = 1 - target[:, -1:]
+                    # CAREFUL that you don't rely on target after this line!
+                    target = target[:, :-1]
+            else:
+                mask = None
+    
+            tp, fp, fn, _ = get_tp_fp_fn_tn(predicted_segmentation_onehot, target, axes=axes, mask=mask)
+    
+            tp_hard = tp.detach().cpu().numpy()
+            fp_hard = fp.detach().cpu().numpy()
+            fn_hard = fn.detach().cpu().numpy()
+            if not self.label_manager.has_regions:
+                # if we train with regions all segmentation heads predict some kind of foreground. In conventional
+                # (softmax training) there needs tobe one output for the background. We are not interested in the
+                # background Dice
+                # [1:] in order to remove background
+                tp_hard = tp_hard[1:]
+                fp_hard = fp_hard[1:]
+                fn_hard = fn_hard[1:]
+    
+            return {'loss': l.detach().cpu().numpy(), 'tp_hard': tp_hard, 'fp_hard': fp_hard, 'fn_hard': fn_hard}
 class nnUNetTrainerDAExtHybrid(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
