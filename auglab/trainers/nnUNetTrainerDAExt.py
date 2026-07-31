bb4ba2cf6b0c9e59af343bafcba33fa6205b9bae
Nathan Molinier <nathan.molinier@polymtl.ca>
2026-07-31 16:24:31 -0400

    implement checkpoint save for different probability of augmentation

--- auglab/trainers/nnUNetTrainerDAExt.py
+++ auglab/trainers/nnUNetTrainerDAExt.py
@@ -1,14 +1,10 @@
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
-from nnunetv2.training.dataloading.data_loader import nnUNetDataLoader
-from nnunetv2.utilities.default_n_proc_DA import get_allowed_n_proc_DA
-from nnunetv2.training.dataloading.nnunet_dataset import infer_dataset_class
-from nnunetv2.utilities.label_handling.label_handling import determine_num_input_channels
-from torch.nn.parallel import DistributedDataParallel as DDP
+from nnunetv2.training.loss.dice import get_tp_fp_fn_tn
+from nnunetv2.utilities.collate_outputs import collate_outputs
 
-from batchgenerators.dataloading.single_threaded_augmenter import SingleThreadedAugmenter
-from batchgenerators.dataloading.nondet_multi_threaded_augmenter import NonDetMultiThreadedAugmenter
 from typing import Tuple, Union, List
 import numpy as np
+from batchgenerators.utilities.file_and_folder_operations import join
 from batchgeneratorsv2.helpers.scalar_type import RandomScalar
 from batchgeneratorsv2.transforms.base.basic_transform import BasicTransform
 from batchgeneratorsv2.transforms.nnunet.random_binary_operator import ApplyRandomBinaryOperatorTransform
@@ -27,6 +23,7 @@ import os
 import torch
 import importlib
 from torch import autocast
+from torch import distributed as dist
 import json
 import shutil
 
@@ -173,6 +170,8 @@ class nnUNetTrainerDAExtGPU(nnUNetTrainer):
         with open(json_path, 'r') as f:
             config = json.load(f)
         self.validation_augmentation_checkpoints = config.get("ValidationAugmentationCheckpoints", {}).get("checkpoints", [0])
+        self.ema_dice_validation = [None] * len(self.validation_augmentation_checkpoints)
+        self.best_ema_dice_validation = [None] * len(self.validation_augmentation_checkpoints)
 
         # Copy json transfrom parameters to output folder
         shutil.copy(
@@ -459,6 +458,80 @@ class nnUNetTrainerDAExtGPU(nnUNetTrainer):
                 fn_hard = fn_hard[1:]
     
             return {'loss': l.detach().cpu().numpy(), 'tp_hard': tp_hard, 'fp_hard': fp_hard, 'fn_hard': fn_hard}
+
+    def compute_validation_metrics(self, val_outputs: List[dict]):
+            '''
+            Based on on_validation_epoch_end nnUNetTrainer
+            '''
+            outputs_collated = collate_outputs(val_outputs)
+            tp = np.sum(outputs_collated['tp_hard'], 0)
+            fp = np.sum(outputs_collated['fp_hard'], 0)
+            fn = np.sum(outputs_collated['fn_hard'], 0)
+    
+            if self.is_ddp:
+                world_size = dist.get_world_size()
+    
+                tps = [None for _ in range(world_size)]
+                dist.all_gather_object(tps, tp)
+                tp = np.vstack([i[None] for i in tps]).sum(0)
+    
+                fps = [None for _ in range(world_size)]
+                dist.all_gather_object(fps, fp)
+                fp = np.vstack([i[None] for i in fps]).sum(0)
+    
+                fns = [None for _ in range(world_size)]
+                dist.all_gather_object(fns, fn)
+                fn = np.vstack([i[None] for i in fns]).sum(0)
+    
+                losses_val = [None for _ in range(world_size)]
+                dist.all_gather_object(losses_val, outputs_collated['loss'])
+                loss_here = np.vstack(losses_val).mean()
+            else:
+                loss_here = np.mean(outputs_collated['loss'])
+    
+            global_dc_per_class = [i for i in [2 * i / (2 * i + j + k) for i, j, k in zip(tp, fp, fn)]]
+            mean_fg_dice = np.nanmean(global_dc_per_class)
+            val_losses = loss_here
+            return mean_fg_dice, global_dc_per_class, val_losses
+    
+    def run_training(self):
+            self.on_train_start()
+    
+            for epoch in range(self.current_epoch, self.num_epochs):
+                self.on_epoch_start()
+    
+                self.on_train_epoch_start()
+                train_outputs = []
+                for batch_id in range(self.num_iterations_per_epoch):
+                    train_outputs.append(self.train_step(next(self.dataloader_train)))
+                self.on_train_epoch_end(train_outputs)
+    
+                with torch.no_grad():
+                    self.on_validation_epoch_start()
+                    val_outputs = [[]]*len(self.validation_augmentation_checkpoints)
+                    for batch_id in range(self.num_val_iterations_per_epoch):
+                        batch = next(self.dataloader_val)
+                        for prob_id, augmentation_prob in enumerate(self.validation_augmentation_checkpoints):
+                            val_outputs[prob_id].append(self.validation_step(batch, augmentation_prob))
+                    for val_idx, val_output in enumerate(val_outputs):
+                        mean_fg_dice, global_dc_per_class, val_losses = self.compute_validation_metrics(val_output)
+                        self.ema_dice_validation[val_idx] = self.ema_dice_validation[val_idx] * 0.9 + 0.1 * mean_fg_dice if self.ema_dice_validation[val_idx] is not None else mean_fg_dice
+                        if val_idx == 0:
+                            self.logger.log('mean_fg_dice', mean_fg_dice, self.current_epoch)
+                            self.logger.log('dice_per_class_or_region', global_dc_per_class, self.current_epoch)
+                            self.logger.log('val_losses', val_losses, self.current_epoch)
+    
+                self.on_epoch_end()
+                for val_idx, augmentation_prob in enumerate(self.validation_augmentation_checkpoints):
+                    ema_dice = self.ema_dice_validation[val_idx]
+                    if ema_dice is None or ema_dice > self.best_ema_dice_validation[val_idx]:
+                        self.best_ema_dice_validation[val_idx] = ema_dice
+                        self.save_checkpoint(join(self.output_folder, f'checkpoint_best_validation_aug_{augmentation_prob}.pth'))
+    
+            self.on_train_end()
+    
+
+
 class nnUNetTrainerDAExtHybrid(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
